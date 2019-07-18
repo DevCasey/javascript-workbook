f2065c2674ac5e727df7a1f8a177a31f5d82ca7d
Casey Calkins <c.angelcalkins@outlook.com>
2019-07-18 12:51:59 -0500

    Finished homework assignment

--- 04week/loop.js
+++ 04week/loop.js
@@ -4,25 +4,25 @@
 //     i++;
 // } while (i <= 100);
 
-let person = [{
-    firstName: "Jane",
-    lastName: "Doe",
-    birthDate: "Jan 5, 1925",
-    gender: "female"
-}]
+// let person = [{
+//     firstName: "Jane",
+//     lastName: "Doe",
+//     birthDate: "Jan 5, 1925",
+//     gender: "female"
+// }]
 
 
-for (person.birthDate in person) {
-    for (let i = 0; i < person.length; i++) {
-        let birthSplit = person[i].birthDate.split(' ');
-        if (birthSplit[2] % 2 !==0) {
-            let birthReturn = birthSplit.join(' ');
-            console.log(birthReturn);
-        } else {
-            console.log('false');
-        }
-     }
-}
+// for (person.birthDate in person) {
+//     for (let i = 0; i < person.length; i++) {
+//         let birthSplit = person[i].birthDate.split(' ');
+//         if (birthSplit[2] % 2 !==0) {
+//             let birthReturn = birthSplit.join(' ');
+//             console.log(birthReturn);
+//         } else {
+//             console.log('false');
+//         }
+//      }
+// }
 
 let arrayOfPersons = [{
     firstName: "Jane",
@@ -32,12 +32,12 @@ let arrayOfPersons = [{
 }, {
     firstName: "Bob",
     lastName: "Chase",
-    birthDate: "Jan 5, 1925",
+    birthDate: "Jan 5, 1825",
     gender: "female"
 }, {
     firstName: "Casey",
     lastName: "Samantha",
-    birthDate: "April 27, 1993",
+    birthDate: "April 27, 1893",
     gender: "female"
 }, {
     firstName: "Gavin",
@@ -47,6 +47,26 @@ let arrayOfPersons = [{
 }]
 
 
-arrayOfPersons.map(element => {
-    console.log(element);
-})
+// arrayOfPersons.map(element => {
+//     console.log(element);
+// });
+
+// const onlyMales = arrayOfPersons.filter(function sorted(person) {
+//     if (person.gender == 'male')
+//     console.log(person.firstName + ' ' + person.lastName)
+// })
+
+// .7
+// const findOldPeople = arrayOfPersons.filter(year => {
+//     let re = /[' ']\d{4}/;
+
+//     let string = year.birthDate;
+
+//     if (string.match(re) <= 1990) {
+//         console.log(year.firstName + ' ' + year.lastName);
+//     }
+// })
+
+
+
+
